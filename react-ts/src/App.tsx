b83adc74bf878e812fe9fcc7a3955f57901a8e2e
xupengfei <faberxu@gmail.com>
2021-09-19 20:28:08 +0800

    A: react-ts

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+export default function App() {
+	return (
+		<div id="app">
+			<div id="nav">
+				<a to="/">Home</a>
+				<a to="/about">About</a>
+			</div>
+		</div>
+	);
+}
