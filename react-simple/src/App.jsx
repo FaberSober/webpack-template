4a10f7f4a3ba40c11369858b9270757687df78b4
xupengfei <faberxu@gmail.com>
2021-09-19 17:04:07 +0800

    react-simple

--- react-simple/src/App.jsx
+++ react-simple/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import './index.scss'
 
 export default function App() {
   return (
-    <div>Hello</div>
+    <div id="app">
+      <div id="nav">
+        <a to="/">Home</a>
+        <a to="/about">About</a>
+        <a to="/vuex">Vuex</a>
+      </div>
+    </div>
   )
 }
