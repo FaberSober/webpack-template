bc9a391c24ae151d8e7dba98fed69f3128dfa9cd
xupengfei <faberxu@gmail.com>
2021-09-19 17:32:36 +0800

    react-simple eslint

--- react-simple/.eslintrc.js
+++ react-simple/.eslintrc.js
@@ -1,11 +1,24 @@
 module.exports = {
+	// parser: "babel-eslint",
 	parserOptions: {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+		"ecmaFeatures": {
+      "jsx": true
+    },
 	},
-	plugins: [],
+	plugins: ["react"],
 	extends: [
+		'plugin:react/recommended',
 		'plugin:prettier/recommended',
 	],
 	rules: {
 		"prettier/prettier": "off",
 	},
+	settings: {
+		// https://github.com/yannickcr/eslint-plugin-react#configuration
+		react: {
+			"version": "detect", // React version. "detect" automatically picks the version you have installed.
+		},
+	}
 };
--- react-simple/src/App.jsx
+++ react-simple/src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import './index.scss'
+import React from 'react';
+import './index.scss';
 
 export default function App() {
-  return (
-    <div id="app">
-      <div id="nav">
-        <a to="/">Home</a>
-        <a to="/about">About</a>
-        <a to="/vuex">Vuex</a>
-      </div>
-    </div>
-  )
+	return (
+		<div id="app">
+			<div id="nav">
+				<a to="/">Home</a>
+				<a to="/about">About</a>
+			</div>
+		</div>
+	);
 }
