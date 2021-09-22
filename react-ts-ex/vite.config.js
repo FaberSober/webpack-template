7634b6c9dd331a33257e538a406f0cb213ad3a5d
xu.pengfei <1508075252@qq.com>
2021-09-22 12:21:37 +0800

    tidy react-ts-ex

--- react-ts-ex/vite.config.js
+++ react-ts-ex/vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-const resolveExternalsPlugin = require('vite-plugin-resolve-externals');
+import createExternal from 'vite-plugin-external';
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    resolveExternalsPlugin(),
+    createExternal({
+      externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+      }
+    }),
     reactRefresh(),
   ],
   resolve: {
     alias: [
+      { find: /^~@/, replacement: path.resolve(__dirname, 'src') },
       { find: '@', replacement: path.resolve(__dirname, 'src') },
     ],
-    externals: {
-      'react': 'React',
-      'react-dom': 'ReactDOM',
-    },
   },
 })
