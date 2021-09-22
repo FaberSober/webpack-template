f03d5d6999b6889852e61e6b029b1e7fc57d21e2
xu.pengfei <1508075252@qq.com>
2021-09-22 12:35:04 +0800

    A: update  react-ts-less-antd

--- react-ts-less-antd/src/main.tsx
+++ react-ts-less-antd/src/main.tsx
@@ -4,34 +4,4 @@ import App from '@/App'
 
 import './index.less'
 
-if(!Array.from){
-  Array.from = function (el) {
-      return Array.apply(this, el);
-  }
-} 
-
-
-/*兼容处理 低版本IE*/
-//
-Array.prototype.find || (Array.prototype.find = function (predicate) { 
-  if (this == null) {
-    throw new TypeError('Array.prototype.find called on null or undefined');
-  }
-  if (typeof predicate !== 'function') {
-    throw new TypeError('predicate must be a function');
-  }
-  var list = Object(this);
-  var length = list.length || 0;
-  var thisArg = arguments[1];
-  var value;
-
-  for (var i = 0; i < length; i++) {
-    value = list[i];
-    if (predicate.call(thisArg, value, i, list)) {
-      return value;
-    }
-  }
-  return null;
-})
-
 ReactDom.render(<App />, document.getElementById('root'));
