954cbf531cd4e22dda8e833469df7b536cddd36a
xu.pengfei <1508075252@qq.com>
2021-09-22 11:23:57 +0800

    remove react-ts-sass

--- react-ts-sass/webpack.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (env, argv) {
-    return argv.NODE_ENV === 'production' ? require('./configs/webpack.prod.config.js') : require('./configs/webpack.dev.config.js');
-  };
-  
