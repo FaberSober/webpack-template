c81ccf7b5b3a6671394a2b2cb9a34162b84390a6
xu.pengfei <1508075252@qq.com>
2021-09-22 16:29:22 +0800

    update react-ts

---
+++ react-ts/tsconfig.d.ts
@@ -0,0 +1,14 @@
+// typescript项目中import 图片时报错：TS2307: Cannot find module ‘...’
+// https://www.cnblogs.com/chen-cong/p/10445635.html
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.ico';
+declare module '*.less';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.css';
