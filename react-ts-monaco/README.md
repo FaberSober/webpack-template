87ded0ab9d7a8658cfb1c0a2c3bb0757e83131d1
xu.pengfei <1508075252@qq.com>
2021-09-22 14:38:48 +0800

    add react-ts-monaco

---
+++ react-ts-monaco/README.md
@@ -0,0 +1,3 @@
+# 不支持IE11
+最后支持IE11的官方版本`0.18.1`。
+参考问题：https://github.com/microsoft/monaco-editor/issues/1790
---
+++ react-ts-monaco/src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import MonacoEditor from 'react-monaco-editor';
+import styles from './index.css';
+
+
+console.log('styles', styles)
+
+export default function App() {
+	const [code, setCode] = useState('// type your code...')
+
+	const options = {
+		selectOnLineNumbers: true
+	};
+
+  function editorDidMount(editor:any, monaco:any) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+
+	function onChange(newValue:any, e:any) {
+    console.log('onChange', newValue, e);
+		setCode(newValue)
+  }
+
+	return (
+		<div className={styles.app}>
+			<MonacoEditor
+        width="800"
+        height="600"
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        onChange={onChange}
+        editorDidMount={editorDidMount}
+      />
+		</div>
+	);
+}
