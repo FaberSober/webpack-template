02ab6d69ffa270b6a7e8a52f2c726c3a43749530
xu.pengfei <1508075252@qq.com>
2021-09-23 09:15:00 +0800

    init react-ts-router

---
+++ react-ts-router/configs/webpack.prod.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const webpackConfigBase = require('./webpack.base.config');
+
+module.exports = merge(webpackConfigBase, {
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		filename: '[name].js',
+		publicPath: '/',
+		environment: {
+			// 是否使用箭头函数
+			arrowFunction: false,
+		},
+	},
+  devtool: 'source-map',
+});
