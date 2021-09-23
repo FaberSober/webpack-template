1986abcebf2866f5d2809f20f608f34ad019978f
xu.pengfei <1508075252@qq.com>
2021-09-23 10:16:15 +0800

    add React.lazy dynamic import

--- react-ts-router/README.md
+++ react-ts-router/README.md
@@ -1,1 +1,3 @@
 > 简单的react+ts项目
+
+使用了@react/router和React.lazy实现路由懒加载
--- react-ts-router/src/pages/PageA.tsx
+++ react-ts-router/src/pages/PageA.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { RouteComponentProps } from '@reach/router'
+
+const OtherComponent = React.lazy(() => import('./PageALoad'));
 
 export default function PageA(_: RouteComponentProps) {
   return (
-    <div>
-      PageA
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <OtherComponent />
+    </Suspense>
   )
 }
