46b72e1068356a206eabb6ce09df9f4412f5e985
xu.pengfei <1508075252@qq.com>
2021-09-23 10:28:53 +0800

    update webpack prod output config

--- react-ts-router/configs/webpack.prod.config.js
+++ react-ts-router/configs/webpack.prod.config.js
@@ -4,14 +4,5 @@ const { merge } = require('webpack-merge');
 const webpackConfigBase = require('./webpack.base.config');
 
 module.exports = merge(webpackConfigBase, {
-	output: {
-		path: path.resolve(__dirname, '../dist'),
-		filename: '[name].js',
-		publicPath: '/',
-		environment: {
-			// 是否使用箭头函数
-			arrowFunction: false,
-		},
-	},
   devtool: 'cheap-module-source-map',
 });
