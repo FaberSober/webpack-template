460c888aa94d5bac2db2d66234e2a1d30bc4c536
xu.pengfei <1508075252@qq.com>
2022-02-11 17:05:36 +0800

    ckeditor5-classic

--- react-ts-ckeditor5-classic/README.md
+++ react-ts-ckeditor5-classic/README.md
@@ -1,1 +1,3 @@
-> 简单的react+ts项目
+> 简单的react+ts+ckeditor5项目
+
+使用`@ckeditor/ckeditor5-build-classic`和`@ckeditor/ckeditor5-react`展示基本的ckeditor编辑器项目
--- react-ts-ckeditor5-classic/vite.config.js
+++ react-ts-ckeditor5-classic/vite.config.js
@@ -18,4 +18,7 @@ export default defineConfig({
       { find: '@', replacement: path.resolve(__dirname, 'src') },
     ],
   },
+  server: {
+    open: true,
+  },
 })
