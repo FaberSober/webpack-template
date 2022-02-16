0b1ab6cffe07496b72ec5dee92e841927376eb36
xu.pengfei <1508075252@qq.com>
2022-02-16 16:19:58 +0800

    tinymce upload image showcase

--- react-ts-tinymce/src/App.tsx
+++ react-ts-tinymce/src/App.tsx
@@ -7,11 +7,11 @@ export default function App() {
 
 	const editorRef = useRef<any>(null);
 
-   const log = () => {
-     if (editorRef.current) {
-       console.log(editorRef.current.getContent());
-     }
-   };
+	const log = () => {
+		if (editorRef.current) {
+			console.log(editorRef.current.getContent());
+		}
+	};
 
 	return (
 		<div>
@@ -23,16 +23,32 @@ export default function App() {
 					height: 500,
 					menubar: false,
 					plugins: [
-						'advlist autolink lists link image charmap print preview anchor',
+						'advlist autolink lists link image imagetools charmap print preview anchor',
 						'searchreplace visualblocks code fullscreen',
 						'insertdatetime table paste help wordcount',
 						'colorpicker textcolor contextmenu',
 					],
-					toolbar: 'undo redo | formatselect | fontsizeselect| ' +
-					'bold italic underline strikethrough |  backcolor | alignleft aligncenter ' +
+					toolbar: 'undo redo | formatselect | fontselect | fontsizeselect | ' +
+					'bold italic underline strikethrough | backcolor | alignleft aligncenter ' +
 					'alignright alignjustify | bullist numlist | outdent indent blockquote | ' +
-					'link unlink image code table | removeformat | help',
-					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+					'link unlink image code table charmap | searchreplace visualblocks removeformat wordcount | help',
+					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+					/* enable title field in the Image dialog*/
+					image_title: true,
+					/* enable automatic uploads of images represented by blob or data URIs*/
+					automatic_uploads: true,
+					/*
+						URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+						images_upload_url: 'postAcceptor.php',
+						here we add custom filepicker only to Image dialog
+					*/
+					file_picker_types: 'image',
+					/* and here's our custom image picker*/
+					/**
+					 * 需要在后台提供对应API的接口，参考：https://www.tiny.cloud/docs/advanced/php-upload-handler/
+					 * 返回数据json格式为：{ location : '/your/uploaded/image/file'}
+					 */
+					images_upload_url: '/api/file/uploadImage',
 				}}
        />
        <button onClick={log}>Log editor content</button>
