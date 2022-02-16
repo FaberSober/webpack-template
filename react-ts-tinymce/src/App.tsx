2402779c6c61fda8e7c91b7c7c0ddeb9f354ce68
xu.pengfei <1508075252@qq.com>
2022-02-16 15:58:33 +0800

    tinymce: update toolbar

--- react-ts-tinymce/src/App.tsx
+++ react-ts-tinymce/src/App.tsx
@@ -25,12 +25,13 @@ export default function App() {
 					plugins: [
 						'advlist autolink lists link image charmap print preview anchor',
 						'searchreplace visualblocks code fullscreen',
-						'insertdatetime media table paste code help wordcount'
+						'insertdatetime table paste help wordcount',
+						'colorpicker textcolor contextmenu',
 					],
-					toolbar: 'undo redo | formatselect | ' +
-					'bold italic backcolor | alignleft aligncenter ' +
-					'alignright alignjustify | bullist numlist outdent indent | ' +
-					'removeformat | help',
+					toolbar: 'undo redo | formatselect | fontsizeselect| ' +
+					'bold italic underline strikethrough |  backcolor | alignleft aligncenter ' +
+					'alignright alignjustify | bullist numlist | outdent indent blockquote | ' +
+					'link unlink image code table | removeformat | help',
 					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 				}}
        />
