127200bcf6be02873861cb43ebc131c9fa73e256
xu.pengfei <1508075252@qq.com>
2022-02-16 15:45:11 +0800

    tinymce use Self-hosted releases

--- react-ts-tinymce/src/App.tsx
+++ react-ts-tinymce/src/App.tsx
@@ -1,12 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useRef } from 'react';
 import './index.css'
 import styles from './index.module.css';
+import { Editor } from '@tinymce/tinymce-react';
 
 export default function App() {
 
+	const editorRef = useRef<any>(null);
+
+   const log = () => {
+     if (editorRef.current) {
+       console.log(editorRef.current.getContent());
+     }
+   };
+
 	return (
 		<div>
-			Hello
+			<Editor
+				tinymceScriptSrc="/plugins/tinymce_5.10.3/tinymce/js/tinymce/tinymce.min.js"
+				onInit={(evt, editor) => editorRef.current = editor}
+				initialValue="<p>This is the initial content of the editor.</p>"
+				init={{
+					height: 500,
+					menubar: false,
+					plugins: [
+						'advlist autolink lists link image charmap print preview anchor',
+						'searchreplace visualblocks code fullscreen',
+						'insertdatetime media table paste code help wordcount'
+					],
+					toolbar: 'undo redo | formatselect | ' +
+					'bold italic backcolor | alignleft aligncenter ' +
+					'alignright alignjustify | bullist numlist outdent indent | ' +
+					'removeformat | help',
+					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+				}}
+       />
+       <button onClick={log}>Log editor content</button>
 		</div>
 	);
 }
