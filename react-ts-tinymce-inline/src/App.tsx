9ddbdf75c276597a77f83b75bae26997bf4f448a
xu.pengfei <1508075252@qq.com>
2022-02-16 17:26:43 +0800

    tinymce-inline

--- react-ts-tinymce-inline/src/App.tsx
+++ react-ts-tinymce-inline/src/App.tsx
@@ -14,44 +14,52 @@ export default function App() {
 	};
 
 	return (
-		<div style={{ paddingTop: 60 }}>
-			<Editor
-				tinymceScriptSrc="/plugins/tinymce_5.10.3/tinymce/js/tinymce/tinymce.min.js"
-				onInit={(evt, editor) => editorRef.current = editor}
-				initialValue="<p>This is the initial content of the editor.</p>"
-				init={{
-					// inline: true,
-					height: 500,
-					menubar: false,
-					plugins: [
-						'advlist autolink lists link image imagetools charmap print preview anchor',
-						'searchreplace visualblocks code fullscreen',
-						'insertdatetime table paste help wordcount',
-						'colorpicker textcolor contextmenu',
-					],
-					toolbar: 'undo redo | formatselect | fontselect | fontsizeselect | ' +
-					'bold italic underline strikethrough | backcolor | alignleft aligncenter ' +
-					'alignright alignjustify | bullist numlist | outdent indent blockquote | ' +
-					'link unlink image code table charmap | searchreplace visualblocks removeformat wordcount | help',
-					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-					/* enable title field in the Image dialog*/
-					image_title: true,
-					/* enable automatic uploads of images represented by blob or data URIs*/
-					automatic_uploads: true,
-					/*
-						URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
-						images_upload_url: 'postAcceptor.php',
-						here we add custom filepicker only to Image dialog
-					*/
-					file_picker_types: 'image',
-					/* and here's our custom image picker*/
-					/**
-					 * 需要在后台提供对应API的接口，参考：https://www.tiny.cloud/docs/advanced/php-upload-handler/
-					 * 返回数据json格式为：{ location : '/your/uploaded/image/file'}
-					 */
-					images_upload_url: '/api/file/uploadImage',
-				}}
-       />
+		<div style={{ paddingTop: 30 }}>
+			{
+				(new Array(100).fill('1')).map((v, i) => (
+					<Editor
+					  key={`editor-${i}`}
+						tinymceScriptSrc="/plugins/tinymce_5.10.3/tinymce/js/tinymce/tinymce.min.js"
+						// onInit={(evt, editor) => editorRef.current = editor}
+						initialValue={`<p>${i} This is the initial content of the editor.</p>`}
+						init={{
+							inline: true,
+							height: 500,
+							menubar: false,
+							plugins: [
+								'advlist autolink lists link image imagetools charmap print preview anchor',
+								'searchreplace visualblocks code fullscreen',
+								'insertdatetime table paste help wordcount',
+								'colorpicker textcolor contextmenu',
+							],
+							toolbar: 'undo redo | formatselect | fontselect | fontsizeselect | ' +
+							'bold italic underline strikethrough | backcolor | alignleft aligncenter ' +
+							'alignright alignjustify | bullist numlist | outdent indent blockquote | ' +
+							'link unlink image code table charmap | searchreplace visualblocks removeformat wordcount | help',
+							content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+							/* enable title field in the Image dialog*/
+							image_title: true,
+							/* enable automatic uploads of images represented by blob or data URIs*/
+							automatic_uploads: true,
+							/*
+								URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+								images_upload_url: 'postAcceptor.php',
+								here we add custom filepicker only to Image dialog
+							*/
+							file_picker_types: 'image',
+							/* and here's our custom image picker*/
+							/**
+							 * 需要在后台提供对应API的接口，参考：https://www.tiny.cloud/docs/advanced/php-upload-handler/
+							 * 返回数据json格式为：{ location : '/your/uploaded/image/file'}
+							 */
+							images_upload_url: '/api/file/uploadImage',
+						}}
+						onChange={(e:any) => console.log('onChange', e)}
+						onBlur={(e:any) => console.log('onBlur', e)}
+					/>
+				))
+			}
+			
        <button onClick={log}>Log editor content</button>
 		</div>
 	);
