6b1eee27fa381fcbc5a81ac215dd2bb73fd9767c
xu.pengfei <1508075252@qq.com>
2022-02-16 16:44:09 +0800

    add react-ts-tinymce-inlne

---
+++ react-ts-tinymce-inline/src/App.tsx
@@ -0,0 +1,58 @@
+import React, { useRef } from 'react';
+import './index.css'
+import styles from './index.module.css';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function App() {
+
+	const editorRef = useRef<any>(null);
+
+	const log = () => {
+		if (editorRef.current) {
+			console.log(editorRef.current.getContent());
+		}
+	};
+
+	return (
+		<div style={{ paddingTop: 60 }}>
+			<Editor
+				tinymceScriptSrc="/plugins/tinymce_5.10.3/tinymce/js/tinymce/tinymce.min.js"
+				onInit={(evt, editor) => editorRef.current = editor}
+				initialValue="<p>This is the initial content of the editor.</p>"
+				init={{
+					// inline: true,
+					height: 500,
+					menubar: false,
+					plugins: [
+						'advlist autolink lists link image imagetools charmap print preview anchor',
+						'searchreplace visualblocks code fullscreen',
+						'insertdatetime table paste help wordcount',
+						'colorpicker textcolor contextmenu',
+					],
+					toolbar: 'undo redo | formatselect | fontselect | fontsizeselect | ' +
+					'bold italic underline strikethrough | backcolor | alignleft aligncenter ' +
+					'alignright alignjustify | bullist numlist | outdent indent blockquote | ' +
+					'link unlink image code table charmap | searchreplace visualblocks removeformat wordcount | help',
+					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+					/* enable title field in the Image dialog*/
+					image_title: true,
+					/* enable automatic uploads of images represented by blob or data URIs*/
+					automatic_uploads: true,
+					/*
+						URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+						images_upload_url: 'postAcceptor.php',
+						here we add custom filepicker only to Image dialog
+					*/
+					file_picker_types: 'image',
+					/* and here's our custom image picker*/
+					/**
+					 * 需要在后台提供对应API的接口，参考：https://www.tiny.cloud/docs/advanced/php-upload-handler/
+					 * 返回数据json格式为：{ location : '/your/uploaded/image/file'}
+					 */
+					images_upload_url: '/api/file/uploadImage',
+				}}
+       />
+       <button onClick={log}>Log editor content</button>
+		</div>
+	);
+}
---
+++ react-ts-tinymce-inline/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  //* css模块化
+  css: {
+    modules: { // css模块化 文件以.module.[css|less|scss]结尾
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      hashPrefix: 'prefix',
+    },
+  },
+  resolve: {
+    alias: [
+      { find: /^~@/, replacement: path.resolve(__dirname, 'src') },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
+  server: {
+    open: true,
+  },
+})
