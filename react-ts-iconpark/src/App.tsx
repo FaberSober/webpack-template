a2a6ff0901c7d408d4151e9ee0bac9f46d7b721e
xu.pengfei <1508075252@qq.com>
2022-03-01 10:14:50 +0800

    add @icon-park/react

--- react-ts-iconpark/src/App.tsx
+++ react-ts-iconpark/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './index.module.css';
+import { AcceptEmail, Home, CarouselVideo, Folder } from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
 
 export default function App() {
 	return (
@@ -10,6 +13,14 @@ export default function App() {
 			</div>
 
 			<div className={styles.image} />
+
+			<h1>IconPark</h1>
+			<div>
+				<Home theme="outline" size="24" fill="#333" />
+				<AcceptEmail theme="outline" size="24" fill="#333"/>
+				<CarouselVideo theme="two-tone" size="32" fill={['#333' ,'#2F88FF']}/>
+				<Folder theme="multi-color" size="32" fill={['#333' ,'#2F88FF' ,'#FFF' ,'#43CCF8']}/>
+			</div>
 		</div>
 	);
 }
