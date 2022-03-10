8b355ee350a75555feccabc4722deedef6ec86af
xu.pengfei <1508075252@qq.com>
2022-03-10 16:11:54 +0800

    vite-library

---
+++ vite-library/lib/main.ts
@@ -0,0 +1,14 @@
+import './style.css'
+
+/**
+ * 
+ */
+class MyLib {
+
+  public add(x:number, y:number):number {
+    return x + y;
+  }
+
+}
+
+export default MyLib
---
+++ vite-library/src/main.ts
@@ -0,0 +1,3 @@
+import MyLib from '..'
+
+console.log('m', new MyLib().add(1, 2))
