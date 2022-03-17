d469ec9f184e910fe4d8f8d4b1419f5723da9be3
xu.pengfei <1508075252@qq.com>
2022-03-17 11:23:15 +0800

    react-ts-cesium not working

--- react-ts-cesium/src/App.tsx
+++ react-ts-cesium/src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './index.module.css';
+import * as Cesium from 'cesium';
+// import "cesium/Build/Cesium/Widgets/"
+// import "cesium/Build/Cesium/Widgets/widgets.css";
+
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODM4YmM5YS02NDYyLTQ2YzAtODM1NS1iODY4OTc4ZDA4ZDMiLCJpZCI6ODM5NjQsImlhdCI6MTY0NjAxNzQ2MH0.V6TFAsawkIQ971B5kidOsH7TbU2GbStAQkdIXV-8QUo";
 
 export default function App() {
+
+	useEffect(() => {
+		// Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+		const viewer = new Cesium.Viewer('cesiumContainer', {
+			terrainProvider: Cesium.createWorldTerrain()
+		});    
+		// Add Cesium OSM Buildings, a global 3D buildings layer.
+		const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
+		// Fly the camera to San Francisco at the given longitude, latitude, and height.
+		viewer.camera.flyTo({
+			destination : Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+			orientation : {
+				heading : Cesium.Math.toRadians(0.0),
+				pitch : Cesium.Math.toRadians(-15.0),
+			}
+		});
+	}, [])
+	
 	return (
 		<div className={styles.app}>
-			<div className={styles.nav}>
-				<a href="/">Home</a>
-				<a href="/about">About</a>
-			</div>
-
-			<div className={styles.image} />
+			<div id="cesiumContainer"></div>
 		</div>
 	);
 }
